4d4100027ec98b84506dec4796a246f50536ea41
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2024-04-01 21:42:18 -0300

    exerecicio

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudacao(nome) {
+    return "Olá " + nome;
+}
---
+++ src/exercicio.ts
@@ -0,0 +1,12 @@
+// Função de multiplicação
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá " + nome;
+}
+
+multiplicar(2, 5)
+saudacao("Sanderson")
